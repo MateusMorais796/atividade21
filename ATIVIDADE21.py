8782b7a8cb4ca88f8ccc3686f8bd476713752d6c
MateusMorais796 <moraiscosta321@gmail.com>
2024-09-18 16:08:45 -0300

    contagem regresiva

---
+++ ATIVIDADE21.py
@@ -0,0 +1,9 @@
+# Crie um programa que faça uma contagem regressiva de 10 até 0 e exiba "FIM!" ao final.
+
+n = 10
+
+while n > -1:
+    print(n)
+    n = n-1
+    if n == -1:
+        print("FIM!")
